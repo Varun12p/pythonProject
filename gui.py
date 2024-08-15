86af2bde27dcda9a7cb87ff719dd8a98e4f109fa
Varun <panjwanivarun12@gmail.com>
2024-08-15 13:06:10 -0400

    day 17

--- gui.py
+++ gui.py
@@ -1,3 +1,31 @@
+import FreeSimpleGUI as sg
 import functions
-import FreeSimpleGUI
 
+label = sg.Text("Enter your todo:")
+input_box = sg.Input(tooltip="Enter todo", key="todo")
+add_button = sg.Button("Add")
+
+window = sg.Window("Your todo app.",
+                   layout=[[label],[input_box,add_button]],
+                   font=("Hello", 20))
+
+while True:
+
+    event,value = window.read()
+    print(event)
+    print(value)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = value['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
+
+
+
+
+window.close()
